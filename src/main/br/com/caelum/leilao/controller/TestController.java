f106044570ce5e3476c872ea5ae29642272844db
rcabral <raphaelcabralnet@gmail.com>
2015-06-10 22:54:21 -0300

    Create TestController.java

---
+++ src/main/br/com/caelum/leilao/controller/TestController.java
@@ -0,0 +1,13 @@
+package br.com.caelum.leilao.controller;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class TestController {
+
+	@Path("/")
+	public void index() {
+	}
+
+}
